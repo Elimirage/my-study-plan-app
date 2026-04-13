f39663f5df5d93209ab69f25fffd49dc9276abb2
El'mira <118663617+Elimirage@users.noreply.github.com>
2026-04-13 22:40:42 +0000

    11

--- ai.py
+++ ai.py
@@ -4,7 +4,7 @@ import json
 import pytesseract
 import streamlit as st
 
-FOLDER_ID = "ajei1gk73d5gp8c3pfm6"
+FOLDER_ID = "aje1eg4679d4a70efre8"
 
 YANDEX_MODEL_LITE = "yandexgpt-lite"
 YANDEX_MODEL_CHAT = "yandexgpt"
